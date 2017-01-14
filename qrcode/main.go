9e7a649e719af2ba4b8f8d119d8c8e34eba62b5c
Pan Qing <pqcom@sina.com>
2017-01-14 14:30:55 +0800

    Add input file and os.Stdin support

--- qrcode/main.go
+++ qrcode/main.go
@@ -10,9 +10,11 @@ import (
 	"strings"
 
 	qrcode "github.com/skip2/go-qrcode"
+	"io/ioutil"
 )
 
 func main() {
+	inFile := flag.String("i", "", "content file, empty for STDIN")
 	outFile := flag.String("o", "", "out PNG file prefix, empty for stdout")
 	size := flag.Int("s", 256, "image size (pixel)")
 	flag.Usage = func() {
@@ -41,12 +43,25 @@ Usage:
 	if *size <= 0 {
 		checkError(fmt.Errorf("Error: value of -s should > 0"))
 	}
-	if len(flag.Args()) == 0 {
-		flag.Usage()
-		checkError(fmt.Errorf("Error: no content given"))
-	}
 
 	content := strings.Join(flag.Args(), " ")
+	if *inFile != "" {
+		b, err := ioutil.ReadFile(*inFile)
+		if err != nil {
+			checkError(err)
+		}
+		content += string(b)
+	} else {
+		b, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			checkError(err)
+		}
+		content += string(b)
+	}
+	if content == "" {
+		flag.Usage()
+		checkError(fmt.Errorf("Error: no content given？"))
+	}
 
 	var err error
 	var q *qrcode.QRCode
