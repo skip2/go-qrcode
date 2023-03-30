eb7491012eec5f8ca6add3daa404ce861be7ca59
Apikee <88746971+apikee@users.noreply.github.com>
2023-03-30 21:52:04 +0200

    Decrease quiet zone size

--- version.go
+++ version.go
@@ -3034,7 +3034,7 @@ func (v qrCodeVersion) symbolSize() int {
 // quietZoneSize returns the number of pixels of border space on each side of
 // the QR Code. The quiet space assists with decoding.
 func (v qrCodeVersion) quietZoneSize() int {
-	return 4
+	return 1
 }
 
 // getQRCodeVersion returns the QR Code version by version number and recovery
