dbb0e45157eda8c1776bd6a70561789214924029
Minh Cuong Tran <minh-cuong.tran@schulergroup.com>
2024-02-23 11:51:22 +0100

    add support for ecc aka recovery level, color and background-color

--- qrcode-restapi/main.go
+++ qrcode-restapi/main.go
@@ -2,22 +2,73 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	qrcode "github.com/skip2/go-qrcode"
 )
 
+// https://stackoverflow.com/questions/54197913/parse-hex-string-to-image-color
+func ParseHexColor(s string) (c color.RGBA, err error) {
+	c.A = 0xff
+	switch len(s) {
+	case 6:
+		_, err = fmt.Sscanf(s, "%02x%02x%02x", &c.R, &c.G, &c.B)
+	case 3:
+		_, err = fmt.Sscanf(s, "%1x%1x%1x", &c.R, &c.G, &c.B)
+		// Double the hex digits:
+		c.R *= 17
+		c.G *= 17
+		c.B *= 17
+	default:
+		err = fmt.Errorf("invalid length, must be 7 or 4")
+
+	}
+	return
+}
+
 func getQRCode(c *gin.Context) {
 
 	var data string = c.Query("data")
 	size, _ := strconv.Atoi(c.Query("size"))
+	var ecc string = strings.ToUpper(c.Query("ecc"))
+	var recovery_level qrcode.RecoveryLevel = qrcode.Low
+	// margin, _ := strconv.Atoi(c.Query("margin")), no margin is supported yet in go-qrcode
+	var color string = c.Query("color")
+	var bgcolor string = c.Query("bgcolor")
+	// qzone, no qzone is supported yet in go-qrcode
+	// format, only png format is supported in go-qrcode
+
+	if (strings.Compare(ecc, "L") == 0) ||
+		(strings.Compare(ecc, "M") == 0) ||
+		(strings.Compare(ecc, "Q") == 0) ||
+		(strings.Compare(ecc, "H") == 0) {
+
+		if strings.Compare(ecc, "L") == 0 {
+			recovery_level = qrcode.Low
+		} else if strings.Compare(ecc, "M") == 0 {
+			recovery_level = qrcode.Medium
+		} else if strings.Compare(ecc, "Q") == 0 {
+			recovery_level = qrcode.High
+		} else if strings.Compare(ecc, "H") == 0 {
+			recovery_level = qrcode.Highest
+		} else {
+			fmt.Fprintf(os.Stderr, "%s\n", "ECC aka RecoveryLevel is not supported")
+		}
+	}
+
+	mycolor, _ := ParseHexColor(color)
+	mybgcolor, _ := ParseHexColor(bgcolor)
 
 	var q *qrcode.QRCode
-	q, err := qrcode.New(data, qrcode.Highest)
+	q, err := qrcode.New(data, recovery_level)
 	checkError(err)
+	q.ForegroundColor = mycolor
+	q.BackgroundColor = mybgcolor
 
 	var png []byte
 	png, err = q.PNG(size)
@@ -38,6 +89,6 @@ func main() {
 func checkError(err error) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
-		os.Exit(1)
+		// os.Exit(1)
 	}
 }
